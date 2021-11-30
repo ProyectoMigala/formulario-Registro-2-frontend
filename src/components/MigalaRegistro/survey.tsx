bd94f76d894b189850ab1a2563f6fab8d6f7245e
Luis Sandoval <alfsand97@gmail.com>
2021-11-29 19:18:38 -0700

    feat: Soporte para parsear markdown en descripciones y titulos

--- src/components/MigalaRegistro/survey.tsx
+++ src/components/MigalaRegistro/survey.tsx
@@ -2,6 +2,8 @@ import migalaSurvey from 'config/migala-registro-survey.json'
 import * as Survey from "survey-react";
 import { handleOnCompleted } from 'service';
 import { birthDateValidator } from './validators';
+// @ts-ignore
+import showdown from 'showdown';
 
 Survey
   .FunctionFactory
@@ -21,6 +23,19 @@ defaultThemeColors["$header-color"] = "#DB7FF0";
 Survey.StylesManager.applyTheme('modern');
 
 MigalaRegistroModel.onComplete.add(handleOnCompleted);
+
+const converter = new showdown.Converter();
+
+MigalaRegistroModel.onTextMarkdown
+  .add((survey, options) => {
+      //convert the markdown text to html
+      let str = converter.makeHtml(options.text);
+      //remove root paragraphs <p></p>
+      str = str.substring(3);
+      str = str.substring(0, str.length - 4);
+      //set html
+      options.html = str;
+  });
 
 export {
   MigalaRegistroModel
