6625049c8ff0597c4401ee4a19e6ec1bb6ac1100
Antonio Huerta Reyes <anhr9728@gmail.com>
2021-12-02 15:11:02 -0600

    Feature: Agrega logo en header y favIcon
    - Agrega el logo con animación de spin en el header
    - Agrega nombre del proyecto en el header
    - Agrega favIcon
    - Agrega animaciones de transición para el header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from 'react';
 
+import WhiteLogo from '../../assets/logo_pm_white.png'
+
 export function Header() {
+
   const [fullSize, setFullSize] = useState(true);
 
   const handleScroll = ()=>{
@@ -11,12 +14,32 @@ export function Header() {
     window.addEventListener('scroll', handleScroll);
   }
 
-  useEffect(initialize, []);
+  const setUpScrollTimeout = () => {
+    setTimeout(() => {
+      window.scrollTo({
+        top: 120,
+        behavior: 'smooth'
+      })
+    }, 3500)
+  }
+
+  useEffect(() => {
+    initialize();
+    setUpScrollTimeout()
+  }, []);
 
 
   return (
     <header className={`App-header ${fullSize ? '' : 'minsize' }`}>
-      <div>LOGO</div>
+      <div className="logo-image-container">
+        <img src={WhiteLogo} className="logo-header-img fadeIn"/>
+        <h4 className="header-title">PROYECTO MIGALA</h4>
+      </div>
+
+      <div className="circles-container">
+        <div className="circle circle-1" />
+        <div className="circle circle-2" />
+      </div>
     </header>
   )
 
